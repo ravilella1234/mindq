b7c7e6481c193f999dd94f05cfcdb07ee121c777
maha <maha@gmail.com>
2020-01-31 17:21:54 +0530

    Added mindQ Project into github

---
+++ Mind/src/com/icici/loans/carloans/FirstAbstract.java
@@ -0,0 +1,21 @@
+package com.icici.loans.carloans;
+
+public abstract class FirstAbstract 
+{
+	int a=10;
+	
+	public void m1()
+	{
+		System.out.println("iam m1 concrete method..");
+	}
+	
+	public abstract void m2();
+	
+	
+
+	public static void main(String[] args) 
+	{
+		//FirstAbstract obj=new FirstAbstract();
+	}
+
+}
