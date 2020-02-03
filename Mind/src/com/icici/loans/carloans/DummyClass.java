8e436c57f0dddc2842aa46d76fce37dff4003e8d
maha <maha@gmail.com>
2020-02-03 17:41:20 +0530

    from california

--- Mind/src/com/icici/loans/carloans/DummyClass.java
+++ Mind/src/com/icici/loans/carloans/DummyClass.java
@@ -2,6 +2,21 @@ package com.icici.loans.carloans;
 
 public class DummyClass {
 
+	int a=10,b=20,d;
+	
+	public void add()
+	{
+		d=a+b;
+		System.out.println("Addition of A & b is : " + d);
+	}
+	
+	public void sub()
+	{
+		d=a-b;
+		System.out.println("Sub of A & B Is : " + d);
+	}
+
+	
 	public static void main(String[] args) {
 		System.out.println("iam first line...");
 		
