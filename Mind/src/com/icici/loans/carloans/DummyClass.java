fbf87d4c9c4b2e05559e655781336610e4836a98
maha <maha@gmail.com>
2020-02-03 17:52:41 +0530

    Added x,y,Z

--- Mind/src/com/icici/loans/carloans/DummyClass.java
+++ Mind/src/com/icici/loans/carloans/DummyClass.java
@@ -1,6 +1,20 @@
 package com.icici.loans.carloans;
 
 public class DummyClass {
+	
+	int x=10,y=20,z;
+	
+	public void add()
+	{
+		z=x+y;
+		System.out.println("Addition of X & Y is : " + z);
+	}
+	
+	public void sub()
+	{
+		z=x-y;
+		System.out.println("Sub of X & Y Is : " + z);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("iam first line...");
