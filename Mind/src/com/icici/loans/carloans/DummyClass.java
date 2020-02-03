45af7ff0868f6dc263936155b1e9a3ebf9eb24b4
maha <maha@gmail.com>
2020-02-03 17:28:01 +0530

    Added operations (ADD , Sub)

--- Mind/src/com/icici/loans/carloans/DummyClass.java
+++ Mind/src/com/icici/loans/carloans/DummyClass.java
@@ -1,6 +1,20 @@
 package com.icici.loans.carloans;
 
 public class DummyClass {
+	
+	int a=10,b=20,c;
+	
+	public void add()
+	{
+		c=a+b;
+		System.out.println("Addition of A & b is : " + c);
+	}
+	
+	public void sub()
+	{
+		c=a-b;
+		System.out.println("Sub of A & B Is : " + c);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("iam first line...");
