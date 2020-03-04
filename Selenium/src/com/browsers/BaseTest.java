1d543101833046d065676f0c71ecfbcf23a1e020
maha <maha@gmail.com>
2020-03-04 18:10:18 +0530

    selenoum

--- Selenium/src/com/browsers/BaseTest.java
+++ Selenium/src/com/browsers/BaseTest.java
@@ -13,31 +13,48 @@ public class BaseTest
 	public static WebDriver driver;
 	public static FileInputStream fis;
 	public static Properties p;
+	public static Properties mainenv;
+	public static Properties subenv;
+	public static String projectPath=System.getProperty("user.dir");
 	
 	public static void init() throws Exception
 	{
-		fis=new FileInputStream("D:\\Feb-2020 Working Directory\\Selenium\\data.properties");
+		fis=new FileInputStream(projectPath+"\\data.properties");
 		p=new Properties();
 		p.load(fis);
+		
+		fis=new FileInputStream(projectPath+"\\environment.properties");
+		mainenv=new Properties();
+		mainenv.load(fis);
+		String e = mainenv.getProperty("env");
+		System.out.println(e);
+		
+		fis=new FileInputStream(projectPath+"\\"+e+".properties");
+		subenv=new Properties();
+		subenv.load(fis);
+		String url = subenv.getProperty("amazonurl");
+		System.out.println(url);
 	}
 	
 	public static void openBrowser(String browser)
 	{
 		if(p.getProperty(browser).equals("chrome")) 
 		{
-			System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\drive\\chromedriver.exe");
+			//System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\drive\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", projectPath+"//drivers//chromedriver.exe");
 			driver=new ChromeDriver();
 		}
 		else if(p.getProperty(browser).equals("firefox"))
 		{
-			System.setProperty("webdriver.gecko.driver", "C:\\Users\\DELL\\Desktop\\drive\\geckodriver.exe");
+			//System.setProperty("webdriver.gecko.driver", "C:\\Users\\DELL\\Desktop\\drive\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", projectPath+"//drivers//geckodriver.exe");
 			driver=new FirefoxDriver();
 		}
 	}
 	
 	public static void navigateUrl(String url)
 	{
-		driver.get(p.getProperty(url));
+		driver.get(subenv.getProperty(url));
 	}
 	
 
